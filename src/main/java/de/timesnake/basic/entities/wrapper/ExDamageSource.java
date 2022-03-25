87de23773f7fe2663087e0db7efba831d8c9f6a2
funzter <cavehive@protonmail.com>
2022-03-25 10:47:06 +0100

    skeleton custom arrows, pathfindel updated location speed

--- src/main/java/de/timesnake/basic/entities/wrapper/ExDamageSource.java
+++ src/main/java/de/timesnake/basic/entities/wrapper/ExDamageSource.java
@@ -4,7 +4,30 @@ import net.minecraft.world.damagesource.DamageSource;
 
 public enum ExDamageSource {
 
-    FIRE(DamageSource.a), LIGHTNING(DamageSource.b), BURN(DamageSource.c), LAVA(DamageSource.d), HOT_FLOOR(DamageSource.e), STUCK(DamageSource.f), CRAMMING(DamageSource.g), DROWN(DamageSource.h), STARVE(DamageSource.i), CACTUS(DamageSource.j), FALL(DamageSource.k), FLY_INTO_WALL(DamageSource.l), OUT_OF_WORLD(DamageSource.m), GENERIC(DamageSource.n), MAGIC(DamageSource.o), WITHER(DamageSource.p), ANVIL(DamageSource.q), FALLING_BLOCK(DamageSource.r), DRAGON_BREATH(DamageSource.s), DRYOUT(DamageSource.t), SWEET_BERRY_BUSH(DamageSource.u), FREEZE(DamageSource.v), FALLING_STALACTITE(DamageSource.w), STALAGMITE(DamageSource.x);
+    FIRE(DamageSource.a),
+    LIGHTNING(DamageSource.b),
+    BURN(DamageSource.c),
+    LAVA(DamageSource.d),
+    HOT_FLOOR(DamageSource.e),
+    STUCK(DamageSource.f),
+    CRAMMING(DamageSource.g),
+    DROWN(DamageSource.h),
+    STARVE(DamageSource.i),
+    CACTUS(DamageSource.j),
+    FALL(DamageSource.k),
+    FLY_INTO_WALL(DamageSource.l),
+    OUT_OF_WORLD(DamageSource.m),
+    GENERIC(DamageSource.n),
+    MAGIC(DamageSource.o),
+    WITHER(DamageSource.p),
+    ANVIL(DamageSource.q),
+    FALLING_BLOCK(DamageSource.r),
+    DRAGON_BREATH(DamageSource.s),
+    DRYOUT(DamageSource.t),
+    SWEET_BERRY_BUSH(DamageSource.u),
+    FREEZE(DamageSource.v),
+    FALLING_STALACTITE(DamageSource.w),
+    STALAGMITE(DamageSource.x);
 
     private final DamageSource damageSource;
 
