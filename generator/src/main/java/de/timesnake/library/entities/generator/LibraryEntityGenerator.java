c3aca3fb3276dff202c37200c2e52cdf17f8b752
funzter <cavehive@protonmail.com>
2022-07-12 21:06:53 +0200

    cleanup, bugfixes, updated git ignore

--- generator/src/main/java/de/timesnake/library/entities/generator/LibraryEntityGenerator.java
+++ generator/src/main/java/de/timesnake/library/entities/generator/LibraryEntityGenerator.java
@@ -1,9 +1,11 @@
 package de.timesnake.library.entities.generator;
 
+import de.timesnake.library.entities.entity.extension.ExEntityHuman;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
 
 import java.io.*;
 import java.util.HashMap;
@@ -93,11 +95,16 @@ public class LibraryEntityGenerator {
     private void generatePlayerExtension() throws IOException, TemplateException {
         Template temp = cfg.getTemplate("templates/ExPlayer.ftl");
 
+        NmsExtensionBasis extension = new NmsExtensionBasis(ExEntityHuman.class, CraftPlayer.class);
+
         Map<String, Object> root = new HashMap<>();
         root.put("moduleName", entityOutputModule + "." + destEntityDir);
         root.put("entityModuleName", entityOutputModule);
         root.put("extensionModuleName", extensionModule);
         root.put("version", VERSION);
+        root.put("wrapperModuleName", WrapperGenerator.WRAPPER_MODULE);
+        root.put("craftName", CraftPlayer.class.getName());
+        root.put("extensionEntity", extension);
 
 
         Writer out =
