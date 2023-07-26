17d8cbba31ac5b72fd8731a405504add89316613
funzter <cavehive@protonmail.com>
2023-07-26 13:24:45 +0200

    added RavagerBuilder

---
+++ src/main/java/de/timesnake/library/entities/entity/RavagerBuilder.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2023 timesnake
+ */
+
+package de.timesnake.library.entities.entity;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.monster.Ravager;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.jetbrains.annotations.Nullable;
+
+public class RavagerBuilder extends RaiderBuilder<Ravager, RavagerBuilder> {
+
+  public RavagerBuilder(Ravager entity) {
+    super(entity);
+  }
+
+  public RavagerBuilder(ServerLevel world, boolean loadDefaultPathfinders, boolean randomizeData) {
+    super(new Ravager(EntityType.RAVAGER, world) {
+      @Override
+      protected void registerGoals() {
+        if (loadDefaultPathfinders) {
+          super.registerGoals();
+        }
+      }
+
+      @Nullable
+      @Override
+      public SpawnGroupData finalizeSpawn(ServerLevelAccessor world, DifficultyInstance difficulty, MobSpawnType spawnReason, @Nullable SpawnGroupData entityData, @Nullable CompoundTag entityNbt) {
+        if (randomizeData) {
+          return super.finalizeSpawn(world, difficulty, spawnReason, entityData, entityNbt);
+        }
+        return entityData;
+      }
+    });
+  }
+}
