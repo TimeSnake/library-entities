2b2c34a5ae759948f0ce5f58a34cc0e72d3023c8
funzter <cavehive@protonmail.com>
2024-05-07 21:30:42 +0200

    add LlamaBuilder, HorseBuilder

---
+++ src/main/java/de/timesnake/library/entities/entity/base/AbstractHorseBuilder.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2023 timesnake
+ */
+
+package de.timesnake.library.entities.entity.base;
+
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+
+public abstract class AbstractHorseBuilder<E extends AbstractHorse, B extends AbstractHorseBuilder<E, B>> extends AnimalBuilder<E, B> {
+
+}
