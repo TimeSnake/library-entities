5837a25d0b9c8f31fa06c2a7d63025b456f3736c
funzter <funzter@mail.timesnake.de>
2024-09-13 17:42:45 +0200

    fix skeleton default goals

--- src/main/java/de/timesnake/library/entities/entity/StrayBuilder.java
+++ src/main/java/de/timesnake/library/entities/entity/StrayBuilder.java
@@ -6,11 +6,15 @@ package de.timesnake.library.entities.entity;
 
 import de.timesnake.library.entities.entity.base.AbstractSkeletonBuilder;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.monster.Stray;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ServerLevelAccessor;
 import org.jetbrains.annotations.Nullable;
 
@@ -48,6 +52,34 @@ public class StrayBuilder extends AbstractSkeletonBuilder<Stray, StrayBuilder> {
       @Override
       public boolean isVehicle() {
         return !neverVehicle && super.isVehicle();
+      }
+
+      @Override
+      public void reassessWeaponGoal() {
+        if (loadDefaultPathfinderGoals) {
+          super.reassessWeaponGoal();
+        } else {
+          if (!this.level().isClientSide) {
+            this.goalSelector.removeGoal(StrayBuilder.this.meleeGoal);
+            this.goalSelector.removeGoal(StrayBuilder.this.bowGoal);
+            ItemStack itemstack = this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, Items.BOW));
+
+            if (itemstack.is(Items.BOW)) {
+              int i = this.getHardAttackInterval();
+
+              if (this.level().getDifficulty() != Difficulty.HARD) {
+                i = this.getAttackInterval();
+              }
+
+              StrayBuilder.this.bowGoal.setMinAttackInterval(i);
+              if (StrayBuilder.this.bowGoal != null) {
+                this.goalSelector.addGoal(4, StrayBuilder.this.bowGoal);
+              }
+            } else if (StrayBuilder.this.meleeGoal != null) {
+              this.goalSelector.addGoal(4, StrayBuilder.this.meleeGoal);
+            }
+          }
+        }
       }
     };
   }
